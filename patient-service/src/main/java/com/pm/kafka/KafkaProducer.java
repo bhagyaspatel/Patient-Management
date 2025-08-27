b7d4929562be08fa331fc6df23684168a310b51a
bhagyaspatel <bhagyaspatel0911@gmail.com>
2025-08-28 00:02:38 +0530

    Adding patient-service as kafka consumer

---
+++ patient-service/src/main/java/com/pm/kafka/KafkaProducer.java
@@ -0,0 +1,38 @@
+package com.pm.kafka;
+
+import com.pm.model.Patient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+import patient.events.PatientEvent;
+
+@Service
+public class KafkaProducer {
+
+    // kafkaTemplate: used for sending/producing message with key(String)-Value(byte[]) pair
+    @Autowired
+    private final KafkaTemplate<String, byte[]> kafkaTemplate;
+
+    private static final Logger log = LoggerFactory.getLogger(KafkaProducer.class);
+
+    public KafkaProducer(KafkaTemplate<String, byte[]> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendEvent(Patient patient){
+        PatientEvent patientEvent = PatientEvent.newBuilder()
+                .setPatientId(patient.getId().toString())
+                .setName(patient.getName())
+                .setEmail(patient.getEmail())
+                .setEventType("PATIENT_CREATED")
+                .build();
+
+        try{
+            kafkaTemplate.send("patient", patientEvent.toByteArray());
+        }catch (Exception e){
+            log.error("Error sending PatientCreated event: {}", patientEvent);
+        }
+    }
+}
